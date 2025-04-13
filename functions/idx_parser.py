5bf56b830fcc0cb5ed12f219a8c328dfea217a0c
SiergiejW <pangymyly@gmail.com>
2025-04-13 17:06:50 +0200

    idx parser as separate file

---
+++ functions/idx_parser.py
@@ -0,0 +1,124 @@
+import os
+import struct
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QStandardItem, QStandardItemModel
+from PyQt6.QtWidgets import (QStyle)
+
+
+def parse_idx_file(main_window, cd_folder):
+    idx_path = os.path.join(cd_folder, "TOMBA2.IDX")
+    dat_path = os.path.join(cd_folder, "TOMBA2.DAT")
+    img_path = os.path.join(cd_folder, "TOMBA2.IMG")
+
+    IDX = open(idx_path, "rb")
+    DAT = open(dat_path, "rb")  # Open the DAT file
+    IMG = open(img_path, "rb")
+
+    # Store the DAT file reference for later use
+    main_window.dat_file = dat_path
+
+    chunk_size = 0x800
+    trailer = 0x700
+
+    model = QStandardItemModel()
+    model.setHorizontalHeaderLabels(["Name"])
+    root_item = model.invisibleRootItem()
+
+    folder_icon = main_window.style().standardIcon(QStyle.StandardPixmap.SP_DirIcon)
+    file_icon = main_window.style().standardIcon(QStyle.StandardPixmap.SP_FileIcon)
+
+    for chunk_index in range(int(os.path.getsize(idx_path) / chunk_size)):
+        IDX.seek(chunk_index * chunk_size)
+        img_start, img_end, dat_start, dat_end, pointer_amount = struct.unpack("<5I", IDX.read(20))
+        # if not any([img_start, img_end, dat_start, dat_end, pointer_amount]):
+        #    continue
+
+        IMG.seek(img_start)
+        imgdata = IMG.read(img_end - img_start)
+
+        DAT.seek(dat_start)
+        datdata = DAT.read(dat_end - dat_start)
+
+        sdat_pointers = [main_window.tuplify(item) for item in
+                         struct.unpack("<{:d}I".format(pointer_amount), IDX.read(pointer_amount * 4))]
+
+        IDX.seek(chunk_index * chunk_size + (chunk_size - trailer))
+        traildata = struct.unpack("<{:d}I".format(trailer >> 2), IDX.read(trailer))
+        trail_list = []
+        for t in range(0, len(traildata), 2):
+            dat_trail_start, dat_trail_end = traildata[t], traildata[t + 1]
+            dat_trail_size = dat_trail_end - dat_trail_start
+            if dat_trail_size != 0:
+                trail_list.append((dat_trail_start, dat_trail_end, dat_trail_size))
+
+        area_item = QStandardItem(folder_icon, f"AREA_{chunk_index:02X}")
+        area_item.setFlags(area_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+        root_item.appendRow(area_item)
+
+        if datdata:
+            sdat_item = QStandardItem(folder_icon, f"{chunk_index:02X}_DATA")
+            sdat_item.setFlags(sdat_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+            area_item.appendRow(sdat_item)
+            for i in range(len(sdat_pointers)):
+                id = sdat_pointers[i][0]
+                offset = sdat_pointers[i][1]
+                filetype = main_window.id_convert(DAT, id, hex(dat_start + offset))
+                file_item = QStandardItem(file_icon, f"{id}-{offset:04X}.{filetype}")
+
+                # Store the additional data in the UserRole (or another role)
+                file_item.setData((id, dat_start, offset), Qt.ItemDataRole.UserRole)
+
+                file_item.setFlags(file_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+
+                if filetype == "SPRT":  # SPRT
+                    file_item.setIcon(main_window.sprt_icon)
+                elif filetype == "TXTD":  # txtd
+                    file_item.setIcon(main_window.txtd_icon)
+                    file_path = f"{dat_start + offset:08X}.txtd"
+                    file_item.setData(file_path, Qt.ItemDataRole.UserRole + 1)  # Store file path in another role
+                elif filetype == "TANP":  # TANP
+                    file_item.setIcon(main_window.tanp_icon)
+                elif filetype == "SMST":  # TANP
+                    file_item.setIcon(main_window.smst_icon)
+                elif filetype == "SCLD":
+                    file_item.setIcon(main_window.scld_icon)
+                elif filetype == "MDAT":
+                    file_item.setIcon(main_window.mdat_icon)
+                elif filetype == "DRWB":
+                    file_item.setIcon(main_window.drwb_icon)
+                elif filetype == "BGMP":
+                    file_item.setIcon(main_window.bgmp_icon)
+                elif filetype == "BETP":
+                    file_item.setIcon(main_window.betp_icon)
+                elif filetype == "ALFD":
+                    file_item.setIcon(main_window.alfd_icon)
+                sdat_item.appendRow(file_item)
+
+        if imgdata:
+            vram_item = QStandardItem(folder_icon, f"{chunk_index:02X}_VRAM")
+            vram_item.setFlags(vram_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+            area_item.appendRow(vram_item)
+            vram_file_item = QStandardItem(file_icon, f"{chunk_index:02X}.vram")
+            vram_file_item.setFlags(vram_file_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+            vram_item.appendRow(vram_file_item)
+
+        if traildata:
+            trail_item = QStandardItem(folder_icon, f"{chunk_index:02X}_TRAIL")
+            trail_item.setFlags(trail_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+            area_item.appendRow(trail_item)
+            for i in range(len(trail_list)):
+                adr, end, sz = trail_list[i]
+                trail_file_item = QStandardItem(file_icon, f"{adr:04X}-{end:04X}.bin")
+                trail_file_item.setFlags(trail_file_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+                trail_item.appendRow(trail_file_item)
+
+        main_window.update_folder_name(area_item)
+        if datdata:
+            main_window.update_folder_name(sdat_item)
+        if imgdata:
+            main_window.update_folder_name(vram_item)
+        if traildata:
+            main_window.update_folder_name(trail_item)
+
+    main_window.tree_view.setModel(model)
+    main_window.tree_view.selectionModel().selectionChanged.connect(main_window.on_tree_selection_changed)
--- gui/main_window.py
+++ gui/main_window.py
@@ -13,6 +13,7 @@ from icons.icons import (icon_window,
 from main import version
 from gui.txtd_viewer import TXTDViewer
 from gui.mdat_viewer import MDATViewer
+from functions.idx_parser import parse_idx_file
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -67,164 +68,10 @@ class MainWindow(QMainWindow):
         initial_treeview_width = int(self.width() * 0.30)
         self.splitter.setSizes([initial_treeview_width, self.width() - initial_treeview_width])
 
-    def count_items(self, item):
-        count = 0
-        for i in range(item.rowCount()):
-            child = item.child(i)
-            if child.hasChildren():
-                count += self.count_items(child)
-            else:
-                count += 1
-        return count
-
-    def update_folder_name(self, item):
-        if item.hasChildren():
-            count = self.count_items(item)
-            item.setText(f"{item.text()} ({count})")
-
-    def open_folder_dialog(self):
-        folder = QFileDialog.getExistingDirectory(self, "Select Folder")
-        if folder:
-            cd_folder = os.path.join(folder, "CD")
-            if os.path.exists(cd_folder):
-                required_files = ["TOMBA2.DAT", "TOMBA2.IDX", "TOMBA2.IMG"]
-                if all(os.path.exists(os.path.join(cd_folder, f)) for f in required_files):
-                    self.folder_info_label.setText(f"Selected Folder: {folder}")
-                    self.parse_idx_file(cd_folder)
-                else:
-                    QMessageBox.critical(self, "Error", "The selected folder does not contain the required TOMBA2 files.")
-            else:
-                QMessageBox.critical(self, "Error", "The selected folder does not contain a 'CD' folder.")
-        else:
-            self.folder_info_label.setText("Select Tomba folder (with BIN, CD, MOVIE)")
-
-    def parse_idx_file(self, cd_folder):
-        idx_path = os.path.join(cd_folder, "TOMBA2.IDX")
-        dat_path = os.path.join(cd_folder, "TOMBA2.DAT")
-        img_path = os.path.join(cd_folder, "TOMBA2.IMG")
-
-        IDX = open(idx_path, "rb")
-        DAT = open(dat_path, "rb")  # Open the DAT file
-        IMG = open(img_path, "rb")
-
-        # Store the DAT file reference for later use
-        self.dat_file = dat_path
-
-        chunk_size = 0x800
-        trailer = 0x700
-
-        model = QStandardItemModel()
-        model.setHorizontalHeaderLabels(["Name"])
-        root_item = model.invisibleRootItem()
-
-        folder_icon = self.style().standardIcon(QStyle.StandardPixmap.SP_DirIcon)
-        file_icon = self.style().standardIcon(QStyle.StandardPixmap.SP_FileIcon)
-
-        for chunk_index in range(int(os.path.getsize(idx_path) / chunk_size)):
-            IDX.seek(chunk_index * chunk_size)
-            img_start, img_end, dat_start, dat_end, pointer_amount = struct.unpack("<5I", IDX.read(20))
-            #if not any([img_start, img_end, dat_start, dat_end, pointer_amount]):
-            #    continue
-
-            IMG.seek(img_start)
-            imgdata = IMG.read(img_end - img_start)
-
-            DAT.seek(dat_start)
-            datdata = DAT.read(dat_end - dat_start)
-
-            sdat_pointers = [self.tuplify(item) for item in
-                             struct.unpack("<{:d}I".format(pointer_amount), IDX.read(pointer_amount * 4))]
-
-            IDX.seek(chunk_index * chunk_size + (chunk_size - trailer))
-            traildata = struct.unpack("<{:d}I".format(trailer >> 2), IDX.read(trailer))
-            trail_list = []
-            for t in range(0, len(traildata), 2):
-                dat_trail_start, dat_trail_end = traildata[t], traildata[t + 1]
-                dat_trail_size = dat_trail_end - dat_trail_start
-                if dat_trail_size != 0:
-                    trail_list.append((dat_trail_start, dat_trail_end, dat_trail_size))
-
-            area_item = QStandardItem(folder_icon, f"AREA_{chunk_index:02X}")
-            area_item.setFlags(area_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
-            root_item.appendRow(area_item)
-
-            if datdata:
-                sdat_item = QStandardItem(folder_icon, f"{chunk_index:02X}_DATA")
-                sdat_item.setFlags(sdat_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
-                area_item.appendRow(sdat_item)
-                for i in range(len(sdat_pointers)):
-                    id = sdat_pointers[i][0]
-                    offset = sdat_pointers[i][1]
-                    filetype = self.id_convert(DAT, id, hex(dat_start + offset))
-                    file_item = QStandardItem(file_icon, f"{id}-{offset:04X}.{filetype}")
-
-                    # Store the additional data in the UserRole (or another role)
-                    file_item.setData((id, dat_start, offset), Qt.ItemDataRole.UserRole)
-
-                    file_item.setFlags(file_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
-
-                    if filetype == "SPRT": #SPRT
-                        file_item.setIcon(self.sprt_icon)
-                    elif filetype == "TXTD": #txtd
-                        file_item.setIcon(self.txtd_icon)
-                        file_path = f"{dat_start + offset:08X}.txtd"
-                        file_item.setData(file_path, Qt.ItemDataRole.UserRole + 1)  # Store file path in another role
-                    elif filetype == "TANP": #TANP
-                        file_item.setIcon(self.tanp_icon)
-                    elif filetype == "SMST": #TANP
-                        file_item.setIcon(self.smst_icon)
-                    elif filetype == "SCLD":
-                        file_item.setIcon(self.scld_icon)
-                    elif filetype == "MDAT":
-                        file_item.setIcon(self.mdat_icon)
-                    elif filetype == "DRWB":
-                        file_item.setIcon(self.drwb_icon)
-                    elif filetype == "BGMP":
-                        file_item.setIcon(self.bgmp_icon)
-                    elif filetype == "BETP":
-                        file_item.setIcon(self.betp_icon)
-                    elif filetype == "ALFD":
-                        file_item.setIcon(self.alfd_icon)
-                    sdat_item.appendRow(file_item)
-
-            if imgdata:
-                vram_item = QStandardItem(folder_icon, f"{chunk_index:02X}_VRAM")
-                vram_item.setFlags(vram_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
-                area_item.appendRow(vram_item)
-                vram_file_item = QStandardItem(file_icon, f"{chunk_index:02X}.vram")
-                vram_file_item.setFlags(vram_file_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
-                vram_item.appendRow(vram_file_item)
-
-            if traildata:
-                trail_item = QStandardItem(folder_icon, f"{chunk_index:02X}_TRAIL")
-                trail_item.setFlags(trail_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
-                area_item.appendRow(trail_item)
-                for i in range(len(trail_list)):
-                    adr, end, sz = trail_list[i]
-                    trail_file_item = QStandardItem(file_icon, f"{adr:04X}-{end:04X}.bin")
-                    trail_file_item.setFlags(trail_file_item.flags() & ~Qt.ItemFlag.ItemIsEditable)
-                    trail_item.appendRow(trail_file_item)
-
-            self.update_folder_name(area_item)
-            if datdata:
-                self.update_folder_name(sdat_item)
-            if imgdata:
-                self.update_folder_name(vram_item)
-            if traildata:
-                self.update_folder_name(trail_item)
-
-        self.tree_view.setModel(model)
-        self.tree_view.selectionModel().selectionChanged.connect(self.on_tree_selection_changed)
-
-    def tuplify(self, item):
-        dat_id = item >> 24
-        dat_ptr = item & 0x00FFFFFF
-        return (dat_id, dat_ptr)
-
-    def id_convert(self, DAT, id, pointer_start):
+    def id_convert(main_window, DAT, id, pointer_start):
         if id == 0:
             return "SPRT"
-        elif id in [2,3]:
+        elif id in [2, 3]:
             return "TXT2"
         elif id == 13:
             return "TXTD"
@@ -259,6 +106,43 @@ class MainWindow(QMainWindow):
                 return "SMST"
         else:
             return "NULL"
+
+    def count_items(self, item):
+        count = 0
+        for i in range(item.rowCount()):
+            child = item.child(i)
+            if child.hasChildren():
+                count += self.count_items(child)
+            else:
+                count += 1
+        return count
+
+    def update_folder_name(self, item):
+        if item.hasChildren():
+            count = self.count_items(item)
+            item.setText(f"{item.text()} ({count})")
+
+    def open_folder_dialog(self):
+        folder = QFileDialog.getExistingDirectory(self, "Select Folder")
+        if folder:
+            cd_folder = os.path.join(folder, "CD")
+            if os.path.exists(cd_folder):
+                required_files = ["TOMBA2.DAT", "TOMBA2.IDX", "TOMBA2.IMG"]
+                if all(os.path.exists(os.path.join(cd_folder, f)) for f in required_files):
+                    self.folder_info_label.setText(f"Selected Folder: {folder}")
+                    parse_idx_file(self, cd_folder)
+                else:
+                    QMessageBox.critical(self, "Error", "The selected folder does not contain the required TOMBA2 files.")
+            else:
+                QMessageBox.critical(self, "Error", "The selected folder does not contain a 'CD' folder.")
+        else:
+            self.folder_info_label.setText("Select Tomba folder (with BIN, CD, MOVIE)")
+
+    def tuplify(self, item):
+        dat_id = item >> 24
+        dat_ptr = item & 0x00FFFFFF
+        return (dat_id, dat_ptr)
+
 
     def setup_tree_view(self):
         self.tree_view.setModel(QStandardItemModel())
